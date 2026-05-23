c157661cd19224aa6947024aa27108335a3887b8
christopheraseidl <73406146+christopheraseidl@users.noreply.github.com>
2026-05-23 14:00:01 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -29,7 +29,7 @@ class TestCase extends Orchestra
 
         $this->loadLaravelMigrations();
 
-        foreach (File::allFiles(__DIR__ . '/../database/migrations') as $migration) {
+        foreach (File::allFiles(__DIR__.'/../database/migrations') as $migration) {
             (include $migration->getRealPath())->up();
         }
     }
